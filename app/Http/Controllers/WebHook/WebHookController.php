42b57d9736437d60f19a7278ca5adbc203b19008
Jacob Atam <jacob.atam@gmail.com>
2020-12-31 08:18:10 +0100

    Updated favicon

--- app/Http/Controllers/WebHook/WebHookController.php
+++ app/Http/Controllers/WebHook/WebHookController.php
@@ -107,7 +107,6 @@ class WebHookController extends Controller
     }
 
     public function gitHub(){
-        //$deploy = base_path()."/deploy.sh";
         $output =  shell_exec('sh /home/forge/ohram.org/deploy.sh');
         Log::info($output);
     }
