f36b36b9d4c81547b6c010390635765cbd28d3b6
Atam Jacob <jacob.atam@gmail.com>
2021-01-30 04:25:45 -0800

    Updated Transaction Log

--- app/Http/Controllers/Transaction/TransactionController.php
+++ app/Http/Controllers/Transaction/TransactionController.php
@@ -58,7 +58,7 @@ class TransactionController extends Controller
     public function confirm(Request $request)
     {
         
-        $prudid = 1076;
+        $prudid = 22125466;
 			
         $parameters = array(
             "productid"=>$request->productId,
@@ -68,8 +68,8 @@ class TransactionController extends Controller
 			
 			$ponmo = http_build_query($parameters);
 				
-			$url = "https://sandbox.interswitchng.com/collections/api/v1/gettransaction.json?" . $ponmo; // json
-			$mac    = "D3D1D05AFE42AD50818167EAC73C109168A0F108F32645C8B59E897FA930DA44F9230910DAC9E20641823799A107A02068F7BC0F4CC41D2952E249552255710F";
+			$url = "https://webpay.interswitchng.com/collections/api/v1/gettransaction.json?" . $ponmo; // json
+			$mac    = "AGYclEQngemQDoUCSJBGzeYro8Keu8rVLVjR1aCsR0Mk0TaAjgiI3UnU1aV9a0fQ96KcGLPDOrHOy3oSDjnUMZEo2NJFFXu1hpnYnwcTrJg1RJdc7fo4bvlzHp8a97DX";
 			$hashv =$prudid . $request->reqRef . $mac;
 			$thash = hash('sha512',$hashv);
 			//note the variables appended to the url as get values for these parameters
