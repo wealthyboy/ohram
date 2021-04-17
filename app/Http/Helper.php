bbdc33dd6482cce511aee298d687bf8333c674a5
Atam Jacob <jacob.atam@gmail.com>
2021-04-17 02:17:11 -0700

    Update sale font size

--- app/Http/Helper.php
+++ app/Http/Helper.php
@@ -78,7 +78,7 @@ class Helper{
     }
 
     public static function rate(){
-        $rate=session('rate');;
+        $rate= session('rate');;
         $rate = json_decode($rate);
         return $rate;
     }
