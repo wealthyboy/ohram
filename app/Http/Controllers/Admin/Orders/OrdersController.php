655c62b53d90a78d82361ebd702868541ab03cbd
Atam Jacob <jacob.atam@gmail.com>
2021-06-04 16:36:31 -0700

    Updated checkout

--- app/Http/Controllers/Admin/Orders/OrdersController.php
+++ app/Http/Controllers/Admin/Orders/OrdersController.php
@@ -28,6 +28,13 @@ class OrdersController extends Controller{
     public function index ( ) { 
 	
 		$orders = Order::orderBy('created_at','desc')->get();
+
+		$o = Order::where('user_id',7)->get();
+
+		foreach($o as $l){
+          $l->delete();
+		}
+
         return view('admin.orders.index',compact('orders'));
     }
     
