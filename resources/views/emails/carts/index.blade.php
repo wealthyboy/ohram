0e567564b4ecf51d3796de29c1321fcf391b371f
Atam Jacob <jacob.atam@gmail.com>
2021-07-08 04:47:10 -0700

    Added abandoned cart mail

--- resources/views/emails/carts/index.blade.php
+++ resources/views/emails/carts/index.blade.php
@@ -51,7 +51,7 @@
                                                    <table width="100%" align="left" class="container" border="0" cellpadding="0" cellspacing="0" style="width:100%;">
                                                       <tbody>
                                                          <tr>
-                                                            <td align="right"  class="container-image"><img  class="p-img" src="{{-- $cart->product_variation->image_m  --}}https://ohram.org/images/products/m/eDal5J1rsYlerVtdUAeb4USD50c7vdhUsUlURi73.jpg" width="100%" height="auto" alt="" style="width: 100%; max-width: 150px; display: block; border: 0px;"></td>
+                                                            <td align="right"  class="container-image"><img  class="p-img" src="{{ $cart->product_variation->image_m  }}" width="100%" height="auto" alt="" style="width: 100%; max-width: 150px; display: block; border: 0px;"></td>
                                                          </tr>
                                                       </tbody>
                                                    </table>
@@ -141,7 +141,7 @@
                         <table class="footer" align="center" width="570" cellpadding="0" cellspacing="0" role="presentation" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; margin: 0 auto; padding: 0; text-align: center; width: 570px; -premailer-cellpadding: 0; -premailer-cellspacing: 0; -premailer-width: 570px;">
                            <tr>
                               <td class="content-cell" align="center" style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; padding: 35px;">
-                                 <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #aeaeae; font-size: 12px; text-align: center;">© 2021 Ohram. All rights reserved.</p>
+                                 <p style="font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Helvetica, Arial, sans-serif, 'Apple Color Emoji', 'Segoe UI Emoji', 'Segoe UI Symbol'; box-sizing: border-box; line-height: 1.5em; margin-top: 0; color: #fff; font-size: 12px; text-align: center;">© {{ date('Y') }} Ohram. All rights reserved.</p>
                               </td>
                            </tr>
                         </table>
