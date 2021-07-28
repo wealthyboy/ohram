af10c45c4e30449280ef108ef0c8539af856e4fc
Atam Jacob <jacob.atam@gmail.com>
2021-07-27 20:21:58 -0700

    Updated Checkout

--- app/Http/Controllers/Checkout/CheckoutController.php
+++ app/Http/Controllers/Checkout/CheckoutController.php
@@ -60,8 +60,7 @@ class CheckoutController extends Controller
 		$user = $request->user();
 
 		
-		AbandonCart::dispatch($carts, $user)->delay(now()->addMinutes(30));
-
+		AbandonCart::dispatch($carts, $user)->delay(now()->addMinutes(10));
 		return view('checkout.index',['csrf' => $csrf]);
 	}
 
