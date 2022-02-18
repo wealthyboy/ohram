e3ac242f990ba20f965edb413789b3b0b5483922
Atam Jacob <jacob.atam@gmail.com>
2022-02-17 19:10:43 -0800

    Updated Cart

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,21 +2,6 @@
  
 @section('content')
 
-<div class="top-notice text-white bg-primary">
-    <div class="container-fluid text-center">
-        <div class="row">
-            <div class="col-12">
-                <h5 class="d-inline-block text-white text-uppercase mb-0">
-                    <b class="text-danger"><i class="fas fa-money-check"></i>
-                    Black friday 25th-26th site wide discount
-                 </b>
-                </h5>
-            </div>
-
-        </div>
-        
-    </div><!-- End .container -->
-</div>
 
 
 
