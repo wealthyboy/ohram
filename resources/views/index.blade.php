c680c4b59aa35d62ed70e7b2976c9627f6e1c16b
Jacob <jacob.atam@gmail.com>
2025-04-10 16:55:04 +0100

    fix

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,35 +1,27 @@
 @extends('layouts.app')
- 
+
 @section('content')
 
 
+<div class="row no-gutters">
+    <div class="col-md-6 bg--primary order-last d-none d-lg-block">
 
+        <div class="d-flex justify-content-center align-content-center">
 
-
-<div class="container-fliud ">
-    
-
-
-
-    <div  class="row no-gutters">
-        <div class="col-md-6 bg--primary order-last d-none d-lg-block">
-
-          <div class="d-flex justify-content-center align-content-center">
-              
-           <div class="sale-content text-center align-content-center  fadeInRight animated">
+            <div class="sale-content text-center align-content-center  fadeInRight animated">
 
                 <div class="text-uppercase fa-3x position-absolute text-white">Tummy Make over</div>
                 <div class="">
-                   <h1  class="text-uppercase  sale   sale-text text-white">Sale</h1>
+                    <h1 class="text-uppercase  sale   sale-text text-white">Sale</h1>
                 </div>
 
                 <div class="">
-                   <h1  class="text-uppercase  bold fa-2x text-white">From 12% off</h1>
+                    <h1 class="text-uppercase  bold fa-2x text-white">From 12% off</h1>
                 </div>
 
 
                 <div class="buttons position-relative ">
-                    <a href="https://ohram.org/products/teatox" class="btn btn-raised  btn--lg btn--primary bold h-font mr-2 mb-1">Shop Tea</a> 
+                    <a href="https://ohram.org/products/teatox" class="btn btn-raised  btn--lg btn--primary bold h-font mr-2 mb-1">Shop Tea</a>
                 </div>
                 <div class="buttons  position-relative ">
                     <a href="https://ohram.org/products/weight-loss-gummies" class="btn btn-raised btn--lg btn-raised btn--primary bold h-font mr-2 mb-1">Shop Gummies</a>
@@ -38,193 +30,82 @@
                     <a href="https://ohram.org/products/weight-loss-pills" class="btn btn-raised btn--lg btn--primary bold h-font mr-2 mb-1">Shop Loss Pills</a>
                 </div>
                 <div class="buttons">
-                <a href="https://ohram.org/products/slim-diet-coffee" class="btn  btn-raised btn--lg btn--primary bold h-font mr-2 mb-1">Shop Coffee</a>
+                    <a href="https://ohram.org/products/slim-diet-coffee" class="btn  btn-raised btn--lg btn--primary bold h-font mr-2 mb-1">Shop Coffee</a>
                 </div>
-                </div>
-          </div>
-
-        </div>
-        <div class="col-md-6 order-first">
-            <div class="custom-banner">
-                <section   class="weight-loss-banner">
-                    <div class="mobile-weight-loss-banner  d-block d-sm-none">
-                        <div class="text-uppercase fa-2x  bold text-white">Tummy Make over</div>
-                            <div class=" text-center">
-                                <h1  class="text-uppercase font-italic sale fa-4x text-white">Sale</h1>
-                            </div>
-                            <div class="text-center">
-                                <h1  class="text-uppercase  bold fa-1x text-white">From 12% off</h1>
-                            </div>
-
-                            <div class="buttons  text-center position-relative ">
-                            <a href="https://ohram.org/products/weight-loss" class="btn   btn--lg btn--primary bold h-font mr-2 mb-1">Shop Weight Loss</a> 
-                            </div>
-                        </div>
-                    </div>
-                    
-                </section>
             </div>
         </div>
+
     </div>
-    <div id="sneakers"  class="row no-gutters  mar-1">
-        <div class="col-md-4   sneakers-content d-flex justify-content-center align-items-center">
-           <div class="sal-content text-left   ml-5 pl-3">
-               <h1  class="text-uppercase  m-0">RUN WITH CONFIDENCE</h1>
-               <p class=" ml-">When the world is your gym, it’s all about the components of your shoe.</p>
-           </div>
-        </div>
-<!-- 
-        <div class="col-md-4  d-flex justify-content-center align-items-center">
-           <div class="sal-content text-left   ml-5 ">
-               <h1  class="text-uppercase m-0">RUN WITH CONFIDENCE</h1>
-               <p class="">When the world is your gym, it’s all about the components of your shoe.</p>
-           </div>
-        </div>  -->
-        <div style="  background-color: #000;" class="col-md-8 sneakers-content">
-            <div  class="custom-banner">
-                <section  style="background-repeat: no-repeat;background-size: contain;  background-color: #000;" class="breadcrumb pt-5 pb-5 justify-content-center">
-                   <div class="banner-sneakers-slider owl-carousel owl-them ">
-                        <div class="item">
-                            <a href="https://ohram.org/product/shop-all/ohram-air-peg">
-                            <img src="/images/banners/buy_sneakers_in_nigeria-removebg-preview.png" alt="" srcset="">
-                            </a>
-                            <div class="text-center">
-                                <a href="">
-                                  <h4 class="text-white  ml-3 bold">OHRAM AIR PEG</h4>
-                                </a>
-                                <div class="price-box text-white mx-auto mt-1"><span class="product-price text-white bold">₦25,000</span></div>
-                            </div>
-                        </div>
-                        <div class="item ">
-                            <a href="https://ohram.org/product/shop-all/ohram-air-zoom">
-                               <img src="/images/banners/buy_ohram_Air_zoom_in_nigeria.png" alt="" srcset="">
-                            </a>
-                            <div class="text-center">
-                                <a href="https://ohram.org/product/shop-all/ohram-air-zoom">
-                                   <h4 class="text-white  ml-3 bold">OHRAM AIR ZOOM</h4>
-                                </a>
-                                <div class="price-box text-white mx-auto mt-1"><span class="product-price text-white bold">₦25,000</span></div>
-                            </div>
-                        </div>
-
-                        <div class="item position-relative">
-                            <a href="https://ohram.org/product/shop-all/vapor">
-                               <img src="/images/banners/OLbY7FJU24hUX9h53nzWKGWoOaeuFGJgkbsHvuKI-removebg-preview.png" alt="" srcset="">
-                            </a>
-                            <div class="text-center">
-                                <a href="https://ohram.org/product/shop-all/vapor">
-                                   <h4 class="text-white  ml-3 bold">VAPOR</h4>
-                                </a>
-                                <div class="price-box text-white mx-auto mt-1"><span class="product-price text-white bold">₦25,000</span></div>
-                            </div>
-                        </div>
-
-                        <div class="item position-relative">
-                            <a href="https://ohram.org/product/shop-all/ohram-zoom-x">
-                            <img src="/images/banners/buy_ohram_Air_zoom_in_nigeria_white-removebg-preview.png" alt="" srcset="">
-                            </a>
-                            <div class=" text-center">
-                                <a href="https://ohram.org/product/shop-all/ohram-zoom-x">
-                                   <h4 class="text-white  ml-3 bold">OHRAM ZOOM X</h4>
-                                </a>
-                                <div class="price-box text-white mx-auto mt-1"><span class="product-price text-white bold">₦25,000</span></div>
-                            </div>
-                        </div>
+    <div class="col-md-6 order-first">
+        <div class="custom-banner">
+            <section class="weight-loss-banner">
+                <div class="mobile-weight-loss-banner  d-block d-sm-none">
+                    <div class="text-uppercase fa-2x  bold text-white">Tummy Make over</div>
+                    <div class=" text-center">
+                        <h1 class="text-uppercase font-italic sale fa-4x text-white">Sale</h1>
                     </div>
-                </section>
-            </div>
+                    <div class="text-center">
+                        <h1 class="text-uppercase  bold fa-1x text-white">From 12% off</h1>
+                    </div>
+
+                    <div class="buttons  text-center position-relative ">
+                        <a href="https://ohram.org/products/weight-loss" class="btn   btn--lg btn--primary bold h-font mr-2 mb-1">Shop Weight Loss</a>
+                    </div>
+                </div>
         </div>
+
+        </section>
     </div>
-
-    <div  class="row no-gutters mar-1 position-relative">
-        
-        <div class="col-md-3 col-12 bg--primary  d-none d-lg-block">
-            
-            <div class="sal-content text-center mobile-weight-loss-banner text-break">
-                <h1  class="text-uppercase  fa-8x">TOP PICKS</h1>               
-                <div class="buttons">
-                   <a href="https://ohram.org/products/female-apparel" class="btn btn--lg border btn--primary bold h-font">Shop Now</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-3   col-12  ">
-            <a href="https://ohram.org/products/female-apparel">
-                <div class="custom-banner">
-                    <section  style=" cursor: pointer; height: 100vh;background-repeat: no-repeat;background-size: cover;background-image: url('https://ohram.org/images/products/r9QOtQz1xTSqOSijiD5ox3vzOXBCr11DLLZpzIwF.jpg'); background-position: center center; background-color: #b4224f;" class="breadcrumb justify-content-center zoom"></section>
-                </div>
-           </a>
-            
-        </div>
-        <div class="col-md-3 col-12  d-none d-lg-block">
-            <a href="https://ohram.org/product/shop-all/a-babe-jacket-blue">
-                <div class="custom-banner">
-                    <section  style="height: 100vh;background-repeat: no-repeat;background-size: cover;background-image: url('/images/banners/ohram_sports_apparel_1.jpeg'); background-position: center center; background-color: #b4224f;" class="breadcrumb justify-content-center  zoom"></section>
-                </div>
-            </a>
-            
-        </div>
-        <div class="col-md-3 col-4  d-none d-lg-block">
-            <a href="https://ohram.org/product/shop-all/sydney-set">
-                <div class="custom-banner">
-                    <section  style="height: 100vh;background-repeat: no-repeat;background-size: cover;background-image: url('/images/banners/ohram_sports_apparel_2.jpeg'); background-position: center center; background-color: #b4224f;" class="breadcrumb justify-content-center zoom"></section>
-                </div>
-            </a>
-            
-        </div>
-
-        <div class="sal-content mobile text-center   d-block d-sm-none">
-            <h1  class="text-uppercase  text-white fa-8x">TOP PICKS</h1>
-            <div class="buttons">
-                <a href="https://ohram.org/products/female-apparel" class="btn btn--lg border btn--primary bold h-font">Shop Now</a>
-            </div>
-        </div>
-    </div>  
+</div>
 </div>
 
-@if ($posts->count()) 
 
-    <div class="blog-section p-3  pt-5 pb-5 bg--gray">
-        <h1 class=" text-center">OHRAM BLOG</h1>
-        <div class="blog-slider owl-carousel owl-theme dots-top">
-            @foreach($posts as $post)
-                <div class="blog inner-quickview inner-icon text-center bg--light">
-                    <div class="img-container">
-                        <a title="{{  $post->title }}" href="{{ route('blog.show',['blog'=> $post->slug]) }}" class="blog-img-link blog">
-                            <img  title="{{  $post->title }}" src="{{ $post->image }}" alt="{{  $post->title }}" />
-                        </a>
-                    </div>
-                    <div class="blog-details p-3">
-                        <p class="info text-center">
-                            <i class="fab fa-clock-o"></i><span>{{ $post->created_at->diffForHumans() }}</span>
-                        </p>
-                        <div class="tag text-center color--primary mb-1">
-                            @foreach($post->attributes as $tag)
-                               <a class="color--primary" href="/blog/tag/{{ $tag->id }}"><i class="fa fa-tags"></i> {{ $tag->name }}</a>
-                            @endforeach
-                        </div>
-                        <h4 class=" text-center pb-5">
-                            <a title="{{  $post->title }}" href="{{ route('blog.show',['blog'=> $post->slug]) }}" class="">
-                                {{ $post->title }}
-                            </a> 
-                        </h4>
-                    </div><!-- End .product-details -->
-                </div>
-            @endforeach
-        </div><!-- End .products-slider -->
 
-        <div class="review-more-button mt-3">
-            <div class="justifiy-content-center  text-center">
-                <a href="/blog" class="btn btn--lg btn--primary bold h-font">Read More</a>
+</div>
+
+@if ($posts->count())
+
+<div class="blog-section p-3  pt-5 pb-5 bg--gray">
+    <h1 class=" text-center">OHRAM BLOG</h1>
+    <div class="blog-slider owl-carousel owl-theme dots-top">
+        @foreach($posts as $post)
+        <div class="blog inner-quickview inner-icon text-center bg--light">
+            <div class="img-container">
+                <a title="{{  $post->title }}" href="{{ route('blog.show',['blog'=> $post->slug]) }}" class="blog-img-link blog">
+                </a>
             </div>
+            <div class="blog-details p-3">
+                <p class="info text-center">
+                    <i class="fab fa-clock-o"></i><span>{{ $post->created_at->diffForHumans() }}</span>
+                </p>
+                <div class="tag text-center color--primary mb-1">
+                    @foreach($post->attributes as $tag)
+                    <a class="color--primary" href="/blog/tag/{{ $tag->id }}"><i class="fa fa-tags"></i> {{ $tag->name }}</a>
+                    @endforeach
+                </div>
+                <h4 class=" text-center pb-5">
+                    <a title="{{  $post->title }}" href="{{ route('blog.show',['blog'=> $post->slug]) }}" class="">
+                        {{ $post->title }}
+                    </a>
+                </h4>
+            </div><!-- End .product-details -->
         </div>
-    </div><!-- End .blog-section -->
+        @endforeach
+    </div><!-- End .products-slider -->
+
+    <div class="review-more-button mt-3">
+        <div class="justifiy-content-center  text-center">
+            <a href="/blog" class="btn btn--lg btn--primary bold h-font">Read More</a>
+        </div>
+    </div>
+</div><!-- End .blog-section -->
 
 @endif
 
 
 
 
-@if ($reviews->count()) 
+@if ($reviews->count())
 
 <div class="products-section p-3  pt-5 pb-5 ">
     <h1 class="text-center">CUSTOMER REVIEWS</h1>
@@ -233,19 +114,18 @@
         @foreach($reviews as $review)
 
         <div class="reviews-details  border inner-quickview inner-icon">
-            <div  class="img-container">
+            <div class="img-container">
                 <a href="/reviews">
-                    <img title="{{ $review->title }}" src="{{ $review->img() }}" alt="{{ $review->title }} ">
                 </a>
             </div>
             <div class="reviews-item-content p-3">
-                
+
                 <div class="text-center">
                     <h4 class="blog-title text-uppercase color--primary mt-4">{{ optional($review->user)->name }} </h4>
-    
+
                     <h5 class="text-uppercase mt-n3"> {{ optional($review->product)->product_name }}</h5>
                 </div>
-                
+
                 <div class="blog-description-content text-center">
                     <p>{{ str_limit($review->description, 100, '....') }}</p>
                 </div>
@@ -253,7 +133,7 @@
                     <i class="fa fa-clock-o"></i><span>{{ $review->created_at->diffForHumans() }}</span>
                 </p>
             </div>
-            
+
         </div>
         @endforeach
     </div><!-- End .products-slider -->
@@ -264,42 +144,41 @@
     </div>
 </div><!-- End .products-section -->
 
-   
+
 @endif
-    
 
 
 
 
 
 
-    
-   
+
+
+
 @endsection
 @section('page-scripts')
-   <script src="/js/waypoints.js?version={{ str_random(6) }}"></script>
+<script src="/js/waypoints.js?version={{ str_random(6) }}"></script>
 
 @stop
 @section('inline-scripts')
 
- function changeColor() {
-    let target = $(".sale")
-    if (target.hasClass('animated heartBeat')) {
-        target.removeClass('animated heartBeat')
-    } else {
-        target.addClass('animated heartBeat')
+function changeColor() {
+let target = $(".sale")
+if (target.hasClass('animated heartBeat')) {
+target.removeClass('animated heartBeat')
+} else {
+target.addClass('animated heartBeat')
 
-    }
- }
- setInterval(changeColor, 1000)
+}
+}
+setInterval(changeColor, 1000)
 
 
-$(function() {      
-    let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
-    if (isMobile) {
-        $('.weight-loss-banner').addClass('banner-filter')
-    }
+$(function() {
+let isMobile = window.matchMedia("only screen and (max-width: 760px)").matches;
+if (isMobile) {
+$('.weight-loss-banner').addClass('banner-filter')
+}
 });
 
 @stop
-
