37a9fa7c479f0815eb119fc702f8564d66273c47
Jacob <jacob.atam@gmail.com>
2025-04-20 22:57:44 +0100

    f

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -18,6 +18,7 @@ use App\User;
 use App\Http\Helper;
 
 use App\Currency;
+
 class HomeController extends Controller
 {
     /**
@@ -26,30 +27,24 @@ class HomeController extends Controller
      * @return \Illuminate\Contracts\Support\Renderable
      */
     public function index(Request $request)
-    {    
-        
-        $site_status =Live::first();
+    {
+
+        $site_status = Live::first();
         $banners =  Banner::banners()->get();
-        $products = Product::where('featured',1)->orderBy('created_at','DESC')->take(8)->get();
-        $reviews  = Review::orderBy('created_at','DESC')->take(4)->get();
-        $posts  =   Information::orderBy('created_at','DESC')->where('blog',true)->take(6)->get();
+        $products = Product::where('featured', 1)->orderBy('created_at', 'DESC')->take(8)->get();
+        $reviews  = Review::orderBy('created_at', 'DESC')->take(4)->get();
+        $posts  =   Information::orderBy('created_at', 'DESC')->where('blog', true)->take(6)->get();
         $page_title = 'Ohram | From detox teas to meal replacement protein shakes, our babes do it all. Get back on track, reduce bloating, and flatten that tummy!';
 
-        $user = User::where("email", "jacob.atam@gmail.com")->first();
-        $user->password = bcrypt(1122334455);
-        $user->save();
 
-
-        if ( empty($site_status->make_live) ) {
-            return view('index',compact('banners','page_title','reviews','products','posts'));
+        if (empty($site_status->make_live)) {
+            return view('index', compact('banners', 'page_title', 'reviews', 'products', 'posts'));
         } else {
             //Show site if admin is logged in
-            if ( auth()->check()  && auth()->user()->isAdmin()){
-                return view('index',compact('banners','page_title','reviews','products','posts'));
+            if (auth()->check()  && auth()->user()->isAdmin()) {
+                return view('index', compact('banners', 'page_title', 'reviews', 'products', 'posts'));
             }
             return view('underconstruction.index');
         }
     }
-
-    
 }
