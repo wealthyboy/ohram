ad6b01354ebe420d1f0d9a9cbbc5ed547dc1a8eb
Jacob <jacob.atam@gmail.com>
2025-04-29 21:21:53 +0100

    fix

--- routes/web.php
+++ routes/web.php
@@ -228,7 +228,7 @@ Route::post('/create-payment-intent', function (Request $request) {
         'amount' => 1000, // ₦10.00 in kobo or $10.00 in cents
         'currency' => 'usd',
         'automatic_payment_methods' => ['enabled' => true],
-        'payment_method_types' => ['card', 'klarna'],
+        'payment_method_types' => ['afterpay_clearpay', 'card'],
 
     ]);
 
