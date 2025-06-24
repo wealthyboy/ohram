966e06a7c945d36996fae5b40ee19df0387019a4
Jacob <jacob.atam@gmail.com>
2025-06-24 11:40:59 +0100

    fix

--- app/Http/Controllers/Products/ProductsController.php
+++ app/Http/Controllers/Products/ProductsController.php
@@ -26,8 +26,8 @@ class ProductsController extends Controller
 {
 
     public function __construct()
-    {	  
-	  $this->settings =  SystemSetting::first();
+    {
+        $this->settings =  SystemSetting::first();
     }
     /**
      * Display a listing of the resource.
@@ -35,36 +35,38 @@ class ProductsController extends Controller
      * @return \Illuminate\Http\Response
      */
 
-    public function  index(Request $request,Category $category,Builder $builder)  {
+    public function  index(Request $request, Category $category, Builder $builder)
+    {
 
-        $page_title = implode(" ",explode('-',$category->slug));
+        $page_title = implode(" ", explode('-', $category->slug));
         $category_attributes = $category->attribute_parents()->has('children')->get();
-        
-            $products = Product::whereHas('categories',function(Builder  $builder) use ($category){
-                $builder->where('categories.name',$category->name);
-            })->filter($request,$this->getFilters($category_attributes))->latest()->paginate($this->settings->products_items_per_page);
-            $products->appends(request()->all());
-            if($request->ajax()) {
-                return response()->json([
-                    'products' => $products->toArray(),
-                    'category_attributes' => $category_attributes->count()
-                ]); 
-            }
 
-        $breadcrumb = $category->name; 
-        return  view('products.index',compact(
+        $products = Product::whereHas('categories', function (Builder  $builder) use ($category) {
+            $builder->where('categories.name', $category->name);
+        })->filter($request, $this->getFilters($category_attributes))->latest()->paginate($this->settings->products_items_per_page);
+        $products->appends(request()->all());
+        if ($request->ajax()) {
+            return response()->json([
+                'products' => $products->toArray(),
+                'category_attributes' => $category_attributes->count()
+            ]);
+        }
+
+        $breadcrumb = $category->name;
+        return  view('products.index', compact(
             'category',
             'page_title',
             'category_attributes',
             'breadcrumb',
             'products'
-        ));     
+        ));
     }
 
-    public function getFilters($category_attributes){
+    public function getFilters($category_attributes)
+    {
         $filters = [];
-        foreach ($category_attributes as $category_attribute){
-           $filters[$category_attribute->attribute->slug] = AttributesFilter::class;
+        foreach ($category_attributes as $category_attribute) {
+            $filters[$category_attribute->attribute->slug] = AttributesFilter::class;
         }
         return $filters;
     }
@@ -77,50 +79,51 @@ class ProductsController extends Controller
      * @param  int  $id
      * @return \Illuminate\Http\Response
      */
-    public function show(Request $request,Category $category,Product $product)  { 
+    public function show(Request $request, Category $category, Product $product)
+    {
         $page_title = "{$product->product_name}";
-        $favorites ='';
-        $data= [];
+        $favorites = '';
+        $data = [];
         foreach ($product->parent_attributes as  $parent_attribute) {
-            if ($parent_attribute->p_attribute_children()){
+            if ($parent_attribute->p_attribute_children()) {
                 $data[$parent_attribute->name] = $parent_attribute->p_attribute_children();
             }
         }
         $attributes =  collect($data);
         $attributes = $attributes->count() && $product->product_type == 'variable' ? $attributes : '{}';
         $product->load(["variants"]);
-    	return view('products.show',compact('category','attributes','product','page_title'));
-    }
-    
-    public function search(Request $request){
-        $filtered_array = $request->only(['q', 'field']);
-		if (empty( $filtered_array['q'] ) )  { 
-			return redirect('/errors');
-        }
-        $breadcrumb = 'Search Results for  ' .$filtered_array['q'] ; 
 
-		if($request->has('q')){
-			$filtered_array = array_filter($filtered_array);
-                $query = Product::whereHas('categories', function( $query ) use ( $filtered_array ){
-                    $query->where('categories.name','like','%' .$filtered_array['q'] . '%')
-                        ->orWhere('products.product_name', 'like', '%' .$filtered_array['q'] . '%')
-                        ->orWhere('products.sku', 'like', '%' .$filtered_array['q'] . '%');
-                })->orWhereHas('brand', function( $query ) use ( $filtered_array ){
-                    $query->where('brands.brand_name', 'like', '%' .$filtered_array['q'] . '%');
-                });
+
+        return view('products.show', compact('category', 'attributes', 'product', 'page_title'));
+    }
+
+    public function search(Request $request)
+    {
+        $filtered_array = $request->only(['q', 'field']);
+        if (empty($filtered_array['q'])) {
+            return redirect('/errors');
         }
-			
+        $breadcrumb = 'Search Results for  ' . $filtered_array['q'];
+
+        if ($request->has('q')) {
+            $filtered_array = array_filter($filtered_array);
+            $query = Product::whereHas('categories', function ($query) use ($filtered_array) {
+                $query->where('categories.name', 'like', '%' . $filtered_array['q'] . '%')
+                    ->orWhere('products.product_name', 'like', '%' . $filtered_array['q'] . '%')
+                    ->orWhere('products.sku', 'like', '%' . $filtered_array['q'] . '%');
+            })->orWhereHas('brand', function ($query) use ($filtered_array) {
+                $query->where('brands.brand_name', 'like', '%' . $filtered_array['q'] . '%');
+            });
+        }
+
         $products = $query->groupBy('products.id')->latest()->paginate($this->settings->products_items_per_page);
         $products->appends(request()->all());
 
-        if($request->ajax())
-        { 
+        if ($request->ajax()) {
             return response()->json([
                 'products' => $products->toArray(),
-            ]); 
+            ]);
         }
-        return view('products.index',compact('products','breadcrumb'));  
+        return view('products.index', compact('products', 'breadcrumb'));
     }
-
-
 }
