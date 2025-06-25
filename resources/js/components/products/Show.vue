038e856496241a2098b6663fb0d29d5e6048a0b6
Jacob <jacob.atam@gmail.com>
2025-06-25 09:44:44 +0100

    fix

--- resources/js/components/products/Show.vue
+++ resources/js/components/products/Show.vue
@@ -8,7 +8,313 @@
       <!-- End .row -->
     </div>
     <!-- End .product-single-container -->
-  
+    <div class="product-single-tabs">
+      <ul class="nav nav-tabs" role="tablist">
+        <li class="nav-item pl-2">
+          <a
+            class="nav-link active"
+            id="product-tab-desc"
+            data-toggle="tab"
+            href="#product-desc-content"
+            role="tab"
+            aria-controls="product-desc-content"
+            aria-selected="true"
+            ><h4>Description</h4></a
+          >
+        </li>
+
+        <li class="nav-item">
+          <a
+            class="nav-link"
+            id="Warranty_Return"
+            data-toggle="tab"
+            href="#Warranty-Return"
+            role="tab"
+            aria-controls="Warranty-Return"
+            aria-selected="false"
+            ><h4>REVIEWS</h4></a
+          >
+        </li>
+      </ul>
+      <div class="tab-content bg--gray">
+        <div
+          class="tab-pane fade show active pl-2"
+          id="product-desc-content"
+          role="tabpanel"
+          aria-labelledby="product-tab-desc"
+        >
+          <div
+            v-html="product.description"
+            class="product-desc-content pl-2 pb-2"
+          ></div>
+          <!-- End .product-desc-content -->
+        </div>
+        <!-- End .tab-pane -->
+        <div
+          class="tab-pane fade fade"
+          id="Warranty-Return"
+          role="tabpanel"
+          aria-labelledby="Warranty-Return"
+        >
+          <div class="product-reviews-content">
+            <div class="row">
+              <div class="col-xl-5 pb-5">
+                <div v-if="$root.loggedIn" class="add-product-review">
+                  <form action="#" class="comment-form m-0">
+                    <h3 class="review-title">Add a Review</h3>
+
+                    <div class="rating-form">
+                      <label for="rating">Your rating</label>
+                      <span class="rating-stars">
+                        <a
+                          class="star-1"
+                          @click="getStarRating($event, 20)"
+                          href="#"
+                          >1</a
+                        >
+                        <a
+                          class="star-2"
+                          @click="getStarRating($event, 40)"
+                          href="#"
+                          >2</a
+                        >
+                        <a
+                          class="star-3"
+                          @click="getStarRating($event, 60)"
+                          href="#"
+                          >3</a
+                        >
+                        <a
+                          class="star-4"
+                          @click="getStarRating($event, 80)"
+                          href="#"
+                          >4</a
+                        >
+                        <a
+                          class="star-5"
+                          @click="getStarRating($event, 100)"
+                          href="#"
+                          >5</a
+                        >
+                      </span>
+
+                      <select
+                        name="rating"
+                        id="rating"
+                        required=""
+                        style="display: none"
+                      >
+                        <option value="">Rate…</option>
+                        <option value="5">Perfect</option>
+                        <option value="4">Good</option>
+                        <option value="3">Average</option>
+                        <option value="2">Not that bad</option>
+                        <option value="1">Very poor</option>
+                      </select>
+                    </div>
+
+                    <div class="row">
+                      <div class="col-md-6 float-left col-xl-12">
+                        <label>Upload Your Photo </label>
+                        <p>
+                          <small class="text-danger bold"
+                            >Images only, And must not be greater than
+                            8mb</small
+                          >
+                        </p>
+
+                        <div
+                          id="m_image"
+                          class="uploadloaded_image text-center pull-left mb-3"
+                        >
+                          <div
+                            @click.prevent="activateFile"
+                            v-if="!profile_photo"
+                            class="upload-text"
+                          >
+                            <a class="activate-file first" href="#">
+                              <img src="/backend/img/upload_icon.png" />
+                              <b>Add Image </b>
+                            </a>
+                          </div>
+                          <div
+                            id="remove_image"
+                            :class="[profile_photo ? '' : 'd-none']"
+                            class="remove_image"
+                          >
+                            <a
+                              class="activate-file"
+                              @click.prevent="undoImage"
+                              href="#"
+                              >Remove</a
+                            >
+                          </div>
+
+                          <img
+                            v-if="profile_photo"
+                            id="stored_image"
+                            class="img-thumnail"
+                            :src="profile_photo"
+                            alt=""
+                          />
+
+                          <input
+                            accept="image/*"
+                            class="upload_input"
+                            data-msg="Upload  your image"
+                            @change="readURL($event)"
+                            type="file"
+                            id="file_upload_input"
+                            name="img"
+                          />
+                          <input
+                            type="hidden"
+                            id=""
+                            data-msg="Uplaod  your art work"
+                            class="file_upload_input stored_image"
+                            name="image"
+                          />
+                        </div>
+                      </div>
+                      <div class="clearfix"></div>
+                      <div class="col-md-6 col-xl-12">
+                        <div class="form-group">
+                          <textarea
+                            id="comment"
+                            v-model="form.description"
+                            name="description"
+                            class="rating_required form-control form-control-sm"
+                            cols="45"
+                            rows="10"
+                            @input="removeError($event)"
+                            @blur="vInput($event)"
+                            aria-required="true"
+                          >
+                          </textarea>
+                          <span
+                            class="help-block error text-danger text-sm-left"
+                            v-if="errors.description"
+                          >
+                            <strong class="text-danger">{{
+                              formatError(errors.description)
+                            }}</strong>
+                          </span>
+                        </div>
+                        <!-- End .form-group -->
+                      </div>
+                    </div>
+
+                    <input
+                      type="submit"
+                      class="btn btn-dark ls-n-15"
+                      value="Submit"
+                    />
+                  </form>
+                </div>
+                <!-- End .add-product-review -->
+                <div
+                  v-if="!$root.loggedIn"
+                  class="review-form-wrapper ml-3 mt-2"
+                >
+                  <button
+                    data-toggle="modal"
+                    data-target="#login-modal"
+                    type="button"
+                    class="bold btn btn--primary btn-round btn-block btn--primary"
+                  >
+                    Add Review
+                  </button>
+                </div>
+              </div>
+              <div class="col-xl-7">
+                <h2
+                  v-if="!loading && reviews.length"
+                  class="review-title text-uppercase"
+                >
+                  {{ meta.total }} Review(s) for <span>This Product</span>
+                </h2>
+
+                <ol class="comment-list">
+                  <li
+                    v-for="review in reviews"
+                    :key="review.id"
+                    class="comment-container"
+                  >
+                    <div class="comment-avatar">
+                      <img
+                        class="avtar"
+                        v-if="review.image"
+                        width="65"
+                        height="65"
+                        :src="review.image"
+                        alt="avtar"
+                      />
+                      <img
+                        class="avtar"
+                        v-else
+                        src="/img/avtar.jpg"
+                        width="65"
+                        height="65"
+                        alt="avtar"
+                      />
+                    </div>
+                    <!-- End .comment-avatar-->
+
+                    <div class="comment-box">
+                      <div class="ratings-container">
+                        <div class="product-ratings">
+                          <span
+                            class="ratings"
+                            :style="{ width: review.rating + '%' }"
+                          ></span
+                          ><!-- End .ratings -->
+                        </div>
+                        <!-- End .product-ratings -->
+                      </div>
+                      <!-- End .ratings-container -->
+
+                      <div class="comment-info mb-1">
+                        <h4 class="avatar-name">{{ review.full_name }}</h4>
+                        - <span class="comment-date">{{ review.date }}</span>
+                      </div>
+                      <!-- End .comment-info -->
+
+                      <div class="comment-text">
+                        <p>{{ review.description }}</p>
+                      </div>
+                      <!-- End .comment-text -->
+                    </div>
+                    <!-- End .comment-box -->
+                  </li>
+                  <!-- comment-container -->
+                </ol>
+                <!-- End .comment-list -->
+                <div
+                  v-if="!loading && meta && meta.total > meta.per_page"
+                  class="pagination-wraper"
+                >
+                  <div class="pagination">
+                    <ul class="pagination-numbers">
+                      <pagination :useUrl="useUrl" :meta="meta" />
+                    </ul>
+                  </div>
+                </div>
+
+                <div
+                  class="text-center bold"
+                  v-if="!loading && !reviews.length"
+                >
+                  No Reviews
+                </div>
+              </div>
+            </div>
+          </div>
+          <!-- End .product-reviews-content -->
+        </div>
+        <!-- End .tab-pane -->
+      </div>
+      <!-- End .tab-content -->
+    </div>
     <!-- End .product-single-tabs -->
     <login-modal />
     <register-modal />
@@ -44,7 +350,7 @@ export default {
       images: [],
       variant_images: [],
       noRating: false,
-      user: {},
+      user: Window.auth,
       file: null,
       quantity: "",
       useUrl: false,
